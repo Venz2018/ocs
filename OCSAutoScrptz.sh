93c66867db15efebe5f61756fb0132d8ab4a9029
Venz2018 <43979177+Venz2018@users.noreply.github.com>
2019-01-15 11:08:37 +0800

    Update OCSAutoScrptz.sh

--- OCSAutoScrptz.sh
+++ OCSAutoScrptz.sh
@@ -111,9 +111,9 @@ service nginx restart
 apt-get -y install zip unzip
 cd /home/vps/public_html
 cd /home/vps/public_html
-wget https://raw.githubusercontent.com/Venz2018/ocs/master/venz.zip
-unzip venz.zip
-rm -f venz.zip
+wget https://raw.githubusercontent.com/Venz2018/ocs/master/Downloads.zip
+unzip Downloads.zip
+rm -f Downloads.zip
 chown -R www-data:www-data /home/vps/public_html
 chmod -R g+rw /home/vps/public_html
 
